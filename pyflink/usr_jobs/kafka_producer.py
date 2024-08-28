441c839e3108a762ee8321b550597e8c8328c0e5
FJ Reig <48714328+fjreig@users.noreply.github.com>
2024-08-28 12:17:37 +0200

    Update kafka_producer.py

--- pyflink/usr_jobs/kafka_producer.py
+++ pyflink/usr_jobs/kafka_producer.py
@@ -10,18 +10,16 @@ from kafka import KafkaProducer
 SLEEP_TIME = 0.5
 
 def generate_sensor_data() -> dict[str, Any]:
-    """Generates random sensor data. It also adds a timestamp for traceability."""
-    sensor_id = random.randint(1, 34)
-    temperature = round(random.uniform(15, 35), 2)
-    pressure = round(random.uniform(950, 1050), 2)
-    vibration = round(random.uniform(0, 10), 2)
+    """Generates random ph plant data. It also adds a timestamp for traceability."""
+    planta_id = random.randint(1, 34)
+    Radiacion = round(random.uniform(0, 1250), 2)
+    Potencia = round(random.uniform(0, 300), 2)
     sensor_data = {
         "message_id": uuid.uuid4().hex,
-        "sensor_id": sensor_id,
+        "planta_id": planta_id,
         "message": {
-            "temperature": temperature,
-            "pressure": pressure,
-            "vibration": vibration,
+            "Radiacion": Radiacion,
+            "Potencia": Potencia
         },
         # utc timestamp
         "timestamp": datetime.now(timezone.utc).isoformat(),
@@ -29,10 +27,6 @@ def generate_sensor_data() -> dict[str, Any]:
     return sensor_data
 
 def main() -> None:
-    """
-    Controls the flow of the producer. It first subscribes to the topic and then
-    generates sensor data and sends it to the topic.
-    """
     producer = KafkaProducer(
         bootstrap_servers="redpanda:9092",
         value_serializer=lambda v: json.dumps(v).encode("utf-8"),
@@ -40,7 +34,7 @@ def main() -> None:
 
     while True:
         sensor_data = generate_sensor_data()
-        producer.send("sensors", value=sensor_data)
+        producer.send("FV", value=sensor_data)
         print(f"Produced: {sensor_data}")
         time.sleep(SLEEP_TIME)
 
