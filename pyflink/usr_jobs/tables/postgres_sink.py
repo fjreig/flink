0e88ab1dc79463fa77fec0c5649185f1b2140052
FJ Reig <48714328+fjreig@users.noreply.github.com>
2024-08-30 11:32:00 +0200

    Update postgres_sink.py

--- pyflink/usr_jobs/tables/postgres_sink.py
+++ pyflink/usr_jobs/tables/postgres_sink.py
@@ -1,5 +1,6 @@
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, EnvironmentSettings
+import os
 
 def main():
     jar_files = [
@@ -19,16 +20,17 @@ def main():
     # Create Kafka Source Table with DDL
     #######################################################################
     src_ddl = """
-        CREATE TABLE sales_usd (
-            seller_id VARCHAR,
-            amount_usd DOUBLE,
-            sale_ts BIGINT,
+        CREATE TABLE fv_table (
+            planta_id VARCHAR,
+            potencia DOUBLE,
+            radiacion DOUBLE,
+            time_ts BIGINT,
             proctime AS PROCTIME()
         ) WITH (
             'connector' = 'kafka',
-            'topic' = 'sales-usd',
+            'topic' = 'fv_plantas',
             'properties.bootstrap.servers' = 'redpanda:9092',
-            'properties.group.id' = 'sales-usd',
+            'properties.group.id' = 'fv_group',
             'scan.startup.mode' = 'earliest-offset',
             'properties.auto.offset.reset' = 'earliest',
             'format' = 'json'
@@ -38,23 +40,21 @@ def main():
     tbl_env.execute_sql(src_ddl)
 
     # create and initiate loading of source Table
-    tbl = tbl_env.from_path('sales_usd')
+    tbl = tbl_env.from_path('fv_table')
 
     print('\nSource Schema')
     tbl.print_schema()
 
     #####################################################################
-    # Define Tumbling Window Aggregate Calculation (Seller Sales Per Minute)
+    # Define Tumbling Window Aggregate Calculation
     #####################################################################
     sql = """
         SELECT
-          seller_id,
-          window_start, 
-          window_end,
-          SUM(amount_usd) * 0.85 AS window_sales
+          planta_id, window_start, window_end,
+          round(avg(potencia),2) AS Potencia, round(avg(radiacion),2) AS Radiacion
         FROM TABLE(
-            TUMBLE(TABLE sales_usd, DESCRIPTOR(proctime), INTERVAL '60' SECONDS))
-        GROUP BY seller_id, window_start, window_end;
+            TUMBLE(TABLE fv_table, DESCRIPTOR(proctime), INTERVAL '60' SECONDS))
+        GROUP BY planta_id, window_start, window_end;
     """
     revenue_tbl = tbl_env.sql_query(sql)
 
@@ -62,28 +62,29 @@ def main():
     revenue_tbl.print_schema()
 
     ###############################################################
-    # Create Kafka Sink Table
+    # Create Postgres Sink Table
     ###############################################################
-    sink_ddl = """
-        CREATE TABLE sales_euros (
-            seller_id VARCHAR,
+    sink_ddl = f"""
+        CREATE TABLE fv_table2 (
+            planta_id VARCHAR,
             window_start TIMESTAMP(3),
             window_end TIMESTAMP(3),
-            window_sales DOUBLE
+            Potencia DOUBLE,
+            Radiacion DOUBLE
         ) WITH (
             'connector' = 'jdbc',
-            'url' = 'jdbc:postgresql://postgres:5432/monitorizacion',
-            'table-name' = 'sales_euros',
-            'username' = 'postgres',
-            'password' = 'postgres'
+            'url' = 'jdbc:postgresql://postgres:{os.environ['POSTGRES_PORT']}/{os.environ['POSTGRES_DB']}',
+            'table-name' = 'fv_table',
+            'username' = '{os.environ['POSTGRES_USER']}',
+            'password' = '{os.environ['POSTGRES_PASSWORD']}'
         )
     """
     tbl_env.execute_sql(sink_ddl)
 
     # write time windowed aggregations to sink table
-    revenue_tbl.execute_insert('sales_euros').wait()
+    revenue_tbl.execute_insert('fv_table2').wait()
 
-    tbl_env.execute('postgres-table-sink')
+    tbl_env.execute('postgtres-table-sink')
 
 if __name__ == '__main__':
     main()
