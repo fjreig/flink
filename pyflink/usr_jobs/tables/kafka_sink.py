e3fa2b6ba5774d3985404bf5d9fa5a1a8f509ade
FJ Reig <48714328+fjreig@users.noreply.github.com>
2024-08-30 11:31:07 +0200

    Update kafka_sink.py

--- pyflink/usr_jobs/tables/kafka_sink.py
+++ pyflink/usr_jobs/tables/kafka_sink.py
@@ -18,16 +18,17 @@ def main():
     # Create Kafka Source Table with DDL
     #######################################################################
     src_ddl = """
-        CREATE TABLE sales_usd (
-            seller_id VARCHAR,
-            amount_usd DOUBLE,
-            sale_ts BIGINT,
+        CREATE TABLE fv_table (
+            planta_id VARCHAR,
+            potencia DOUBLE,
+            radiacion DOUBLE,
+            time_ts BIGINT,
             proctime AS PROCTIME()
         ) WITH (
             'connector' = 'kafka',
-            'topic' = 'sales-usd',
+            'topic' = 'fv_plantas',
             'properties.bootstrap.servers' = 'redpanda:9092',
-            'properties.group.id' = 'sales-usd',
+            'properties.group.id' = 'fv_group',
             'scan.startup.mode' = 'earliest-offset',
             'properties.auto.offset.reset' = 'earliest',
             'format' = 'json'
@@ -37,7 +38,7 @@ def main():
     tbl_env.execute_sql(src_ddl)
 
     # create and initiate loading of source Table
-    tbl = tbl_env.from_path('sales_usd')
+    tbl = tbl_env.from_path('fv_table')
 
     print('\nSource Schema')
     tbl.print_schema()
@@ -47,24 +48,11 @@ def main():
     #####################################################################
     sql = """
         SELECT
-          seller_id,
-          TUMBLE_END(proctime, INTERVAL '60' SECONDS) AS window_end,
-          SUM(amount_usd) * 0.85 AS window_sales
-        FROM sales_usd
-        GROUP BY
-          TUMBLE(proctime, INTERVAL '60' SECONDS),
-          seller_id
-    """
-
-    sql = """
-        SELECT
-          seller_id,
-          window_start, 
-          window_end,
-          SUM(amount_usd) * 0.85 AS window_sales
+          planta_id, window_start, window_end,
+          round(avg(potencia),2) AS Potencia, round(avg(radiacion),2) AS Radiacion
         FROM TABLE(
-            TUMBLE(TABLE sales_usd, DESCRIPTOR(proctime), INTERVAL '60' SECONDS))
-        GROUP BY seller_id, window_start, window_end;
+            TUMBLE(TABLE fv_table, DESCRIPTOR(proctime), INTERVAL '60' SECONDS))
+        GROUP BY planta_id, window_start, window_end;
     """
     revenue_tbl = tbl_env.sql_query(sql)
 
@@ -75,14 +63,15 @@ def main():
     # Create Kafka Sink Table
     ###############################################################
     sink_ddl = """
-        CREATE TABLE sales_euros (
-            seller_id VARCHAR,
+        CREATE TABLE fv_table2 (
+            planta_id VARCHAR,
             window_start TIMESTAMP(3),
             window_end TIMESTAMP(3),
-            window_sales DOUBLE
+            Potencia DOUBLE,
+            Radiacion DOUBLE
         ) WITH (
             'connector' = 'kafka',
-            'topic' = 'sales-euros',
+            'topic' = 'fv_table',
             'properties.bootstrap.servers' = 'redpanda:9092',
             'format' = 'json'
         )
@@ -90,7 +79,7 @@ def main():
     tbl_env.execute_sql(sink_ddl)
 
     # write time windowed aggregations to sink table
-    revenue_tbl.execute_insert('sales_euros').wait()
+    revenue_tbl.execute_insert('fv_table2').wait()
 
     tbl_env.execute('kafka-table-sink')
 
