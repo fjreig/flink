740ef11bacd9e14ef89fbeafe89a3f13a3b43a16
FJ Reig <48714328+fjreig@users.noreply.github.com>
2024-08-30 11:09:57 +0200

    Update postgres_sink.py

--- pyflink/usr_jobs/tables/postgres_sink.py
+++ pyflink/usr_jobs/tables/postgres_sink.py
@@ -83,7 +83,7 @@ def main():
     # write time windowed aggregations to sink table
     revenue_tbl.execute_insert('sales_euros').wait()
 
-    tbl_env.execute('windowed-sales-euros')
+    tbl_env.execute('postgres-table-sink')
 
 if __name__ == '__main__':
     main()
